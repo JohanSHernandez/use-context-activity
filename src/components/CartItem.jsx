06041c883a2491015567c60d8d70de2b8e1e594a
Johan Hernandez <estiven4758mail.com>
2025-05-13 14:23:21 -0500

    Correccion de lectura de imagenes y distribucion de boton limpiar carrito

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -8,10 +8,10 @@ const CartItem = ({ item }) => {
 
   return (
     <div className="cart-item">
-      <img src={item.image} alt={item.name} className="cart-item-img" />
+      <img src={item.imgURL} alt={item.name} className="cart-item-img" />
       <div className="cart-item-details">
         <h4>{item.name}</h4>
-        <p>Size: {item.size} | Color: {item.color}</p>
+        <p><b>Marca:</b> {item.brand}</p>
         <div className="cart-item-actions">
           <button onClick={() => removeFromCart(item.id, 1)}>-</button>
           <span>{item.quantity}</span>
